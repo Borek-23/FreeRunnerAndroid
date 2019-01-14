aed2d84536cbd421e9f4971d4dc267b5861da627
Borek-23 <23.svejda@gmail.com>
2019-01-14 10:42:13 +0000

    attempted to implement navigation drawer, cannot finish, run out of time

--- MyApplication/app/src/main/java/com/example/borek/myapplication/HistoryActivity.java
+++ MyApplication/app/src/main/java/com/example/borek/myapplication/HistoryActivity.java
@@ -44,26 +44,29 @@ public class HistoryActivity extends AppCompatActivity {
 
         // Create objects for data
         HistoricalData run1 = new HistoricalData(calorieResult, timeResult, distanceResult, dateFormat.format(date));
-//        HistoricalData run2 = new HistoricalData(calorieResult, timeResult, distanceResult, "1/1/2019");
-//        HistoricalData run3 = new HistoricalData("520", "23:23", "9", "3/1/2019");
-//        HistoricalData run4 = new HistoricalData("234", "23:23", "9", "4/1/2019");
-//        HistoricalData run5 = new HistoricalData("234", "23:23", "9", "5/1/2019");
-//        HistoricalData run6 = new HistoricalData("345", "23:23", "9", "6/1/2019");
-//        HistoricalData run7 = new HistoricalData("556", "23:23", "9", "7/1/2019");
-//        HistoricalData run8 = new HistoricalData("456", "23:23", "9", "8/1/2019");
-//        HistoricalData run9 = new HistoricalData("977", "23:23", "9", "9/1/2019");
+        HistoricalData run2 = new HistoricalData(calorieResult, timeResult, distanceResult, dateFormat.format(date));
+        HistoricalData run3 = new HistoricalData(calorieResult, timeResult, distanceResult, dateFormat.format(date));
+        HistoricalData run4 = new HistoricalData(calorieResult, timeResult, distanceResult, dateFormat.format(date));
+        HistoricalData run5 = new HistoricalData(calorieResult, timeResult, distanceResult, dateFormat.format(date));
+        HistoricalData run6 = new HistoricalData(calorieResult, timeResult, distanceResult, dateFormat.format(date));
+        HistoricalData run7 = new HistoricalData(calorieResult, timeResult, distanceResult, dateFormat.format(date));
+        HistoricalData run8 = new HistoricalData(calorieResult, timeResult, distanceResult, dateFormat.format(date));
+        HistoricalData run9 = new HistoricalData(calorieResult, timeResult, distanceResult, dateFormat.format(date));
+        HistoricalData run10 = new HistoricalData(calorieResult, timeResult, distanceResult, dateFormat.format(date));
+
 
         // Create an arrayList to hold the data -> then populate it
         ArrayList<HistoricalData> historyData = new ArrayList<>();
         historyData.add(run1);
-//        historyData.add(run2);
-//        historyData.add(run3);
-//        historyData.add(run4);
-//        historyData.add(run5);
-//        historyData.add(run6);
-//        historyData.add(run7);
-//        historyData.add(run8);
-//        historyData.add(run9);
+        historyData.add(run2);
+        historyData.add(run3);
+        historyData.add(run4);
+        historyData.add(run5);
+        historyData.add(run6);
+        historyData.add(run7);
+        historyData.add(run8);
+        historyData.add(run9);
+        historyData.add(run10);
 
         HistoricalDataListAdapter adapter = new HistoricalDataListAdapter(this, R.layout.adapter_view_layout, historyData);
         resultsListView.setAdapter(adapter);
